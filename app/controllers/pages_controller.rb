bb541c33f4a12ca5593b3fb0d25230d288f38020
natefoxr <nate@foxr.xyz>
2022-08-07 01:46:41 -0700

    Finish linking blogposts to homepage and add small styling updates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,16 @@
 class PagesController < ApplicationController
+    before_action :set_article, only: [:show]
+
     def home
+      @articles = Article.all
     end
+
     def about
     end
+
+    private
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
 end
